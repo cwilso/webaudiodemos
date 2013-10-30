4f723cbfb3dacd13a9b01a19e0a339f78aa71102
Chris Wilson <cwilso@gmail.com>
2013-10-30 15:00:09 +0900

    Prettier.

--- js/ui.js
+++ js/ui.js
@@ -32,12 +32,25 @@ function draw() {
   ctx.moveTo(0,256);
   ctx.lineTo(512,256);
   ctx.stroke();
+  ctx.save();
+  ctx.strokeStyle = "#006644";
+  ctx.beginPath();
+  ctx.setLineDash([5]);
+  ctx.moveTo(0,64);
+  ctx.lineTo(512,64);
+  ctx.stroke();
+  ctx.moveTo(0,192);
+  ctx.lineTo(512,192);
+  ctx.stroke();
+
+  ctx.restore();
+  ctx.beginPath();
   ctx.strokeStyle = "blue";
   ctx.moveTo(0,128);
   ctx.lineTo(512,128);
   ctx.stroke();
 
-  ctx.strokeStyle = "black";
+  ctx.strokeStyle = "white";
 
   ctx.beginPath();
   ctx.moveTo(0,256-data[0]);
