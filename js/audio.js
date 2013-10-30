3809aa6e8f1dc69ba28138b41e7d5398aabd795f
Chris Wilson <cwilso@gmail.com>
2013-10-30 12:59:13 +0900

    Initial commit

---
+++ js/audio.js
@@ -0,0 +1,101 @@
+var audioContext = null;
+var myBuffer = null;
+
+var osc = null;
+function setDutyCycle(amt) {
+	this.delay.delayTime.value = amt/this.frequency;	
+	this.dcGain.gain.value = 1.7*(0.5-amt);
+}
+function start(time) {
+	this.osc1.start(time);
+	this.osc2.start(time);
+	this.dcOffset.start(time);
+}
+function stop(time) {
+	this.osc1.stop(time);
+	this.osc2.stop(time);
+	this.dcOffset.stop(time);
+}
+
+function createDCOffset() {
+	var buffer=audioContext.createBuffer(1,1024,audioContext.sampleRate);
+	var data = buffer.getChannelData(0);
+	for (var i=0; i<1024; i++)
+		data[i]=1.0;
+	var bufferSource=audioContext.createBufferSource();
+	bufferSource.buffer=buffer;
+	bufferSource.loop=true;
+	return bufferSource;
+}
+
+function createPWMOsc(freq, dutyCycle) {
+	var pwm = new Object();
+	var osc1 = audioContext.createOscillator();
+	var osc2 = audioContext.createOscillator();
+	var inverter = audioContext.createGain();
+	var output = audioContext.createGain();
+	var delay = audioContext.createDelay();
+	inverter.gain.value=-1;
+	osc1.type="sawtooth";
+	osc2.type="sawtooth";
+	osc1.frequency.value=freq;
+	osc1.frequency.value=freq;
+	osc1.connect(output);
+	osc2.connect(inverter);
+	inverter.connect(delay);
+	delay.connect(output);
+	var dcOffset = createDCOffset();
+	var dcGain = audioContext.createGain();
+	dcOffset.connect(dcGain);
+	dcGain.connect(output);
+
+	output.gain.value = 0.5;  // purely for debugging.
+
+	pwm.osc1=osc1;
+	pwm.osc2=osc2;
+	pwm.output=output;
+	pwm.delay=delay;
+	pwm.frequency = freq;
+	pwm.dcGain=dcGain;
+	pwm.dcOffset=dcOffset;
+	pwm.setDutyCycle = setDutyCycle;
+	pwm.start=start;
+	pwm.stop=stop;
+
+	pwm.setDutyCycle(dutyCycle);
+	return pwm;
+}
+
+var pwmOsc;
+
+function setupAudio( obj ) {
+	window.AudioContext = window.AudioContext || window.webkitAudioContext;
+	audioContext = new AudioContext();
+
+	obj.analyser = audioContext.createAnalyser();
+	obj.analyser.fftSize = 1024;
+/*
+	osc = audioContext.createOscillator();
+	osc.type = "sawtooth";
+	*/
+
+	pwmOsc=createPWMOsc(440,.5);
+
+	pwmOsc.output.connect(audioContext.destination);
+	pwmOsc.output.connect(obj.analyser);
+	pwmOsc.start(audioContext.currentTime+0.05);
+
+/*
+	var request = new XMLHttpRequest();
+	request.open("GET", "sounds/techno.wav", true);
+	request.responseType = "arraybuffer";
+	request.onload = function() {
+	  audioContext.decodeAudioData( request.response, function(buffer) { 
+	    	myBuffer = buffer;
+	    	appendOutput( "Sound ready." );
+		} );
+	}
+	request.send();
+*/
+
+}
---
+++ js/ui.js
@@ -0,0 +1,118 @@
+
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame    ||
+  window.oRequestAnimationFrame      ||
+  window.msRequestAnimationFrame     ||
+  function( callback ){
+    window.setTimeout(callback, 1000 / 60);
+  };
+})();
+
+function pushme() {
+  pwmOsc.stop(0);
+  //window.cancelAnimationFrame(rafID);
+}
+var rafID;
+
+function draw() {
+  var ctx = this.myContext;
+  var data = new Uint8Array(512);
+  this.analyser.getByteTimeDomainData(data);
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 1;
+
+  ctx.clearRect(0,0,512,256); 
+  ctx.beginPath();
+  ctx.moveTo(0,0);
+  ctx.lineTo(512,0);
+  ctx.stroke();
+  ctx.moveTo(0,256);
+  ctx.lineTo(512,256);
+  ctx.stroke();
+  ctx.strokeStyle = "blue";
+  ctx.moveTo(0,128);
+  ctx.lineTo(512,128);
+  ctx.stroke();
+
+  ctx.strokeStyle = "black";
+
+  ctx.beginPath();
+  ctx.moveTo(0,256-data[0]);
+
+  var zeroCross = findFirstPositiveZeroCrossing(data, 512);
+  if (zeroCross==0)
+    zeroCross=1;
+ 
+  for (var i=zeroCross, j=0; j<(512-zeroCross); i++, j++)
+    ctx.lineTo(j,256-data[i]);
+
+  ctx.stroke();
+  rafID = requestAnimFrame( draw.bind(this) );
+}
+
+function setupCanvas( container ) {
+  var canvas = document.createElement( 'canvas' );
+  canvas.width = 512; 
+  canvas.height = 256; 
+  canvas.myContext = canvas.getContext( '2d' );
+
+  if (container)
+    container.appendChild( canvas );
+  else
+    document.body.appendChild( canvas );
+  return canvas;
+}
+
+function init(){
+  myCanvas = setupCanvas();
+  setupAudio( myCanvas );
+
+  draw.bind(myCanvas)();
+}
+
+window.addEventListener("load", init );
+
+var MINVAL = 134;  // 128 == zero.  MINVAL is the "minimum detected signal" level.
+
+function findFirstPositiveZeroCrossing(buf, buflen) {
+  var i = 0;
+  var last_zero = -1;
+  var t;
+
+  // advance until we're zero or negative
+  while (i<buflen && (buf[i] > 128 ) )
+    i++;
+
+  if (i>=buflen)
+    return 0;
+
+  // advance until we're above MINVAL, keeping track of last zero.
+  while (i<buflen && ((t=buf[i]) < MINVAL )) {
+    if (t >= 128) {
+      if (last_zero == -1)
+        last_zero = i;
+    } else
+      last_zero = -1;
+    i++;
+  }
+
+  // we may have jumped over MINVAL in one sample.
+  if (last_zero == -1)
+    last_zero = i;
+
+  if (i==buflen)  // We didn't find any positive zero crossings
+    return 0;
+
+  // The first sample might be a zero.  If so, return it.
+  if (last_zero == 0)
+    return 0;
+
+  return last_zero;
+}
+
+function dutycyclechange() {
+  pwmOsc.setDutyCycle(parseFloat(document.getElementById("dutycycle").value));
+}
