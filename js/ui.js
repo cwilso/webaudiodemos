0cd69ac4b83ba73a12845853ec5095f22c8c0425
Chris Wilson <cwilso@gmail.com>
2014-05-19 13:15:16 -0700

    Added frequency graph.

--- js/ui.js
+++ js/ui.js
@@ -9,32 +9,76 @@ function pushme() {
 }
 var rafID;
 var myOscilloscope = null;
+var scopeCanvas = null;
+var freqCanvas = null;
+
+function drawFreqBars(analyser,context){
+  var SPACING = 3;
+  var BAR_WIDTH = 1;
+  var canvasWidth = 1024;
+  var canvasHeight = 256;
+  var numBars = Math.round(canvasWidth / SPACING);
+  var freqByteData = new Uint8Array(analyser.frequencyBinCount);
+
+  analyser.getByteFrequencyData(freqByteData); 
+
+  context.clearRect(0, 0, canvasWidth, canvasHeight);
+  context.lineCap = 'round';
+  var multiplier = analyser.frequencyBinCount / numBars;
+
+  // Draw rectangle for each frequency bin.
+  for (var i = 0; i < numBars; ++i) {
+    var magnitude = 0;
+    var offset = Math.floor( i * multiplier );
+    // gotta sum/average the block, or we miss narrow-bandwidth spikes
+    for (var j = 0; j< multiplier; j++)
+      magnitude += freqByteData[offset + j];
+    magnitude = magnitude / multiplier;
+    var magnitude2 = freqByteData[i * multiplier];
+    context.fillStyle = "hsl( " + Math.round((i*360)/numBars) + ", 100%, 50%)";
+    context.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude);
+  }
+}
 
 function draw() {  
-  if (myOscilloscope)
-    myOscilloscope.draw(myCanvas.myContext);
+  if (myOscilloscope) {
+    myOscilloscope.draw(scopeCanvas.myContext);
+    if (freqCanvas)
+      drawFreqBars(myOscilloscope.analyser,freqCanvas.context);
+  }
 
   rafID = requestAnimationFrame( draw );
 }
 
-function setupCanvas( container ) {
-  var canvas = document.createElement( 'canvas' );
-  canvas.width = 512; 
-  canvas.height = 256; 
-  canvas.myContext = canvas.getContext( '2d' );
+function setupCanvases( container ) {
+  scopeCanvas = document.createElement( 'canvas' );
+  scopeCanvas.width = 512; 
+  scopeCanvas.height = 256; 
+  scopeCanvas.id = "scope";
+  scopeCanvas.myContext = scopeCanvas.getContext( '2d' );
 
   if (container)
-    container.appendChild( canvas );
+    container.appendChild( scopeCanvas );
   else
-    document.body.appendChild( canvas );
-  return canvas;
+    document.body.appendChild( scopeCanvas );
+
+  freqCanvas = document.createElement( 'canvas' );
+  freqCanvas.width = 1024; 
+  freqCanvas.height = 256; 
+  freqCanvas.id = "freqbars";
+  freqCanvas.context = freqCanvas.getContext( '2d' );
+
+  if (container)
+    container.appendChild( freqCanvas );
+  else
+    document.body.appendChild( freqCanvas );
 }
 
 function init(){
-  myCanvas = setupCanvas();
-  setupAudio( myCanvas );
+  setupCanvases();
+  setupAudio( scopeCanvas );
 
-  draw.bind(myCanvas)();
+  draw();
 }
 
 window.addEventListener("load", init );
